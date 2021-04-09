f6ad2fa16ae3fbfe7b4ff3c76da321611c58abc9
Ghostlium <gbmtechnologic@gmail.com>
2021-04-08 19:03:42 -0700

    finished https://onemonth.com/courses/python/steps/cheers-it-s-a-challenge

--- python/happy_hour.py
+++ python/happy_hour.py
@@ -10,9 +10,11 @@ people = ["Mattan",
           "Chris",
           "that person you forgot to text back",
           "Kanye West",
-          "Samule L. Jackson"]
+          "Samuel L. Jackson",
+          "Joe Mama"]
 
 random_bar = random.choice(bars)
 random_person = random.choice(people)
+second_random_person = random.choice(people)
 
-print(f"How about you go to {random_bar} with {random_person}")
+print(f"How about you go to {random_bar} with {random_person} and {second_random_person}")
