85d49a4a12875f28afb5534221fcbf05b2d61e9c
Ghostlium <gbmtechnologic@gmail.com>
2021-04-09 12:28:24 -0700

    finished https://onemonth.com/courses/python/steps/python-strings-examples

---
+++ python/strings.py
@@ -0,0 +1,19 @@
+# Strings are text surrounded by quotes
+# Both single (') or double (") or triple (""") quotes are used
+# examples: "dinosaurs", '2112', "I'm lovin' it!"
+
+kanye_quote = "my greatest pain in live is that I will never be able to see myself perform live"
+
+print("kanye_quote") 
+
+kanye_quote = """My greatest pain in life
+is that I will never be able
+to see myself perform live."""
+
+print("kanye_quote") 
+
+hamilton_quote = "Well, the word got around, they said, \"This kid's insane, man\""
+
+print(hamilton_quote)
+
+print("""Well, the word got around, they said, "This kid's insane, man""")
