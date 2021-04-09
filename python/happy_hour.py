e6b61866d9a77e29faf3377fe2d3cb700d34497a
Ghostlium <gbmtechnologic@gmail.com>
2021-04-08 19:04:29 -0700

    added a question mark to the file

--- python/happy_hour.py
+++ python/happy_hour.py
@@ -17,4 +17,4 @@ random_bar = random.choice(bars)
 random_person = random.choice(people)
 second_random_person = random.choice(people)
 
-print(f"How about you go to {random_bar} with {random_person} and {second_random_person}")
+print(f"How about you go to {random_bar} with {random_person} and {second_random_person}?")
