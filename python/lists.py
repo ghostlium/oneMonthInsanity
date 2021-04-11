e32cd77379682f60b27ea3b3aad5a2cc927ffc6a
Ghostlium <gbmtechnologic@gmail.com>
2021-04-10 20:02:55 -0700

    finished https://onemonth.com/courses/python/steps/python-add-remove-elements-list

--- python/lists.py
+++ python/lists.py
@@ -5,3 +5,26 @@ random_things = ["Puppies", 55, "Anthony Weiner", 1/2, ["Oh no", "A list inside 
 # this for-loop goes through a list
 for number in the_count:
     print("this is count", number)
+
+# we can also build lists, first let's start with an empty one
+people = []
+
+people.append("Mattan")
+people.append("Sarah")
+people.append("Chris")
+
+# now we can print them out too
+print(people)
+
+# and remove some
+people.remove("Sarah")
+print(people)
+
+for person in people:
+    print("Person is:", person)
+
+square = 0
+for x in range(10):
+    squareNum = x+1
+    square = squareNum*squareNum
+    print(f"the square of {squareNum} is {square}")
