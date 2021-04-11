efac304737625c379de7a70bd103f605f08c8f25
Ghostlium <gbmtechnologic@gmail.com>
2021-04-10 20:38:18 -0700

    finished https://onemonth.com/courses/python/steps/consult-the-dictionary

---
+++ python/dictionaries.py
@@ -0,0 +1,8 @@
+# states = ['NY', 'PA', 'CA']
+# states = ['New York', 'Pennsylvania', 'California']
+# states = ['New York', 'NY', 'Pennsylvania', 'PA', 'California', 'CA']
+
+states = {'NY': 'New York', 'PA': 'Pennsylvania', 'CA': 'California'}
+
+# user = ['Mattan', 70, 10.5, 'Brown', 'Brown']
+user = {'name': 'Mattan', 'height': 70, 'shoe size': 10.5, 'hair': 'Brown', 'eyes': 'Brown'}
