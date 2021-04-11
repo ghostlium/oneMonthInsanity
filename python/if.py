6699b0febfde949404862358f48299ab79bc31fd
Ghostlium <gbmtechnologic@gmail.com>
2021-04-10 19:44:16 -0700

    finished https://onemonth.com/courses/python/steps/python-if-else-elif-statements-tutorial

--- python/if.py
+++ python/if.py
@@ -3,3 +3,7 @@ answer = input("Do you want to hear a joke? ")
 if answer == "Yes":
     print("I'm against picketing, but I don't know how to show it.")
     # Mitch Hedberg (RIP)
+elif answer == "No":
+    print("Fine.")
+else:
+    print("I don't understand.")
