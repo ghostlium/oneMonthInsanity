5566a1b6f0839181c2d3a6b75285e96d19d7df04
Ghostlium <gbmtechnologic@gmail.com>
2021-04-10 20:42:08 -0700

    finished https://onemonth.com/courses/python/steps/you-ve-got-a-list-in-my-dictionary

--- python/dictionaries.py
+++ python/dictionaries.py
@@ -4,5 +4,33 @@
 
 states = {'NY': 'New York', 'PA': 'Pennsylvania', 'CA': 'California'}
 
+print(states[NY])
+
 # user = ['Mattan', 70, 10.5, 'Brown', 'Brown']
 user = {'name': 'Mattan', 'height': 70, 'shoe size': 10.5, 'hair': 'Brown', 'eyes': 'Brown'}
+
+animal_sounds = {
+
+   "cat": ["meow", "purr"],
+
+   "dog": ["woof", "bark"],
+
+   "fox": []
+
+}
+
+mattan = {'name': 'Mattan', 'height': 70, 'shoe size': 10.5, 'hair': 'Brown', 'eyes': 'Brown'}
+
+chris = {'name': 'Chris', 'height': 68, 'shoe size': 10, 'hair': 'Brown', 'eyes': 'Brown'}
+
+sarah = {'name': 'Sarah', 'height': 72, 'shoe size': 8, 'hair': 'Brown', 'eyes': 'Brown'}
+
+print(people)
+
+for person in people:
+
+    print(person['height'])
+
+for person in people:
+
+    print(person.get('height'))
