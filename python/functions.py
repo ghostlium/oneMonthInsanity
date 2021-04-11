e1062d0962bb6aeb4a8cbf490832fcd4e50acaac
Ghostlium <gbmtechnologic@gmail.com>
2021-04-10 20:46:11 -0700

    finished https://onemonth.com/courses/python/steps/python-functions-tutorial

---
+++ python/functions.py
@@ -0,0 +1,12 @@
+def greet(name):
+    return f"Hey {name}!"
+print(greet('joe'))
+
+def age_in_dog_years(age):
+    return age * 7
+
+print(age_in_dog_years(28))
+
+def concat(w1, w2):
+    return w1+w2
+print(concat("da","nk"))
