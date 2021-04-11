7324d2505e3ab89a0075d90a13f8c0f21c7bfc72
Ghostlium <gbmtechnologic@gmail.com>
2021-04-10 19:54:23 -0700

    finished https://onemonth.com/courses/python/steps/learn-in-in-Python

--- python/if.py
+++ python/if.py
@@ -1,9 +1,12 @@
 answer = input("Do you want to hear a joke? ")
 
-if answer == "Yes" or answer == "yes":
+affirmative_responses = ["yes", "y"]
+negative_responses = ["no", "n"]
+
+if answer.lower() in affirmative_responses:
     print("I'm against picketing, but I don't know how to show it.")
-    # Mitch Hedberg (RIP)
-elif answer == "No" or answer == "no" :
+    # Mitch Hedburg (RIP)
+elif answer.lower() in negative_responses:
     print("Fine.")
 else:
     print("I don't understand.")
