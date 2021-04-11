a273311ed581888799e8d2f756687c1c060c2658
Ghostlium <gbmtechnologic@gmail.com>
2021-04-10 20:51:05 -0700

    finished https://onemonth.com/courses/python/curriculum

--- python/functions.py
+++ python/functions.py
@@ -10,3 +10,7 @@ print(age_in_dog_years(28))
 def concat(w1, w2):
     return w1+w2
 print(concat("da","nk"))
+
+def uppercase_and_reverse(input):
+    return input.upper()[::-1]
+print(uppercase_and_reverse("Do not go gentle into that good night."))
