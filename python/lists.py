698b2789b433c3c88bb50253cf9db754c95b848c
Ghostlium <gbmtechnologic@gmail.com>
2021-04-10 19:58:52 -0700

    finished https://onemonth.com/courses/python/steps/for-looping-python

---
+++ python/lists.py
@@ -0,0 +1,7 @@
+the_count = [1, 2, 3, 4, 6]
+stocks = ["AAPL", "GOOG", "TSLA"]
+random_things = ["Puppies", 55, "Anthony Weiner", 1/2, ["Oh no", "A list inside a list"]]
+
+# this for-loop goes through a list
+for number in the_count:
+    print("this is count", number)
